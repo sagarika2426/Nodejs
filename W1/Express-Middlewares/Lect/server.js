a49c7fc2c194716b262c85cd2939502d3e98c72f
sagarika sahoo <sagarikasahoo16@gmail.com>
2024-04-16 23:47:36 +0530

    added auth middleware

--- W1/Express-Middlewares/Lect/server.js
+++ W1/Express-Middlewares/Lect/server.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json())//req.body as json for POST, PUT, PATH
+
+//Authentication Middleware
+const authMiddleware = (req, res, next) => {
+    if(!req.headers["accesstoken"]){
+        return res.status(401).send("You are not allowed to access this ")
+    }
+    next(); //proceed with the next set of operation
+    //always should be on last line
+
+}
+
+ app.use(authMiddleware);
+
+//Logger Middleware
+const loggerMiddleware = (req, res, next) => {
+
+}
+
 
 app.get("/", (req, res) => {
-    console.log("GET /", req.url);
+    console.log("GET:", req.url);
     res.send("GET / route")
 })
 
